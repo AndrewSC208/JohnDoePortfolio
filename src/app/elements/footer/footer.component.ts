5129ab471624f20453ba8d4c19004341e00e67ad
unknown <meilingandrew@gmail.com>
2017-05-23 19:52:34 -0700

    Added social icons, update header size

--- src/app/elements/footer/footer.component.ts
+++ src/app/elements/footer/footer.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-	title: String = 'This is the text';
-	
+	title: String = "Add social icon links"
+
+	iconList: any = [
+		{	type: 'linkedIn', link: 'https://www.linkedin.com/in/andrew-meiling-87396729/', iconHref: 'test'},
+		{	type: 'github', link: 'https://github.com/AndrewSC208', iconHref: 'test'},
+		{	type: 'twitter', link: 'https://twitter.com/meilingandrew?lang=en', iconHref: 'test'},
+	]
+
   constructor() { }
 
   ngOnInit() {
